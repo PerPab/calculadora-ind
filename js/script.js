501f604153a927d13e1ae19269ac8c1c5e46187c
Pablo <percara.pablo@gmail.com>
2022-12-31 10:05:08 -0300

    manejoDeErrores

--- js/script.js
+++ js/script.js
@@ -1,36 +1,52 @@
 
     
- window.addEventListener('load', function(){
+ $(document).ready( function(){
 
-    let boton = document.getElementById('boton');
+    
+    
     function calcular(){
-        let ingreso = Number(document.getElementById('ingreso').value);
-        let salida = Number(document.getElementById('salida').value);
-        let sueldo = Number(document.getElementById('sueldo-bruto').value);
-        let preAviso = document.getElementById('pre-aviso').value;
+
+        let ingreso =Number( $('#ingreso').val());
+        let salida = Number($('#salida').val());
+        let sueldo = Number($('#sueldo-bruto').val());
+        let preAviso = $('#pre-aviso').val();
         let dineroPreAviso;
         let aniosTrabajados = salida - ingreso;
         let monto;
 
-        if(aniosTrabajados > 6){
-            monto = 60;
-        }else if(aniosTrabajados > 1 && aniosTrabajados < 6){
-            monto = 30;
-        }else {
-            monto = 15;
-        }
-
-        if(preAviso=='no'){
-            dineroPreAviso = (sueldo/30) * monto;
+        if(ingreso=="" || salida == "" || sueldo == ""){
+            $('#resultado').val(`Debe completar todos los campos`);
         }else{
-            dineroPreAviso = 0;
-        }
-          
-        let dineroApagar = (sueldo * aniosTrabajados) + dineroPreAviso;
+            if(!isNaN(ingreso) && !isNaN(salida) && !isNaN(sueldo)){
+                if(aniosTrabajados > 6){
+                    monto = 60;
+                }else if(aniosTrabajados > 1 && aniosTrabajados < 6){
+                    monto = 30;
+                }else {
+                    monto = 15;
+                }
 
-        document.getElementById('resultado').value = `$ ${dineroApagar}`;
+                if(preAviso=='no'){
+                    dineroPreAviso = (sueldo/30) * monto;
+                }else{
+                    dineroPreAviso = 0;
+                }
+            
+                let dineroApagar = (sueldo * aniosTrabajados) + dineroPreAviso;
+
+                $('#resultado').val( `$ ${dineroApagar}`);
+                }
+            else{
+                $('#resultado').val( `Uno de los valores ingresados no es un número`);
+
+
+            }
+        }
+        
     }
 
-    boton.onclick = calcular;
+    $('#boton').click(function(){
+        calcular();
+    })
 
- },true);
+ });
